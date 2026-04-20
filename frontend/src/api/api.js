55c2d5aa249f75821098da559a9c94c5a836d8f7
rkk416 <ramkrishn416@gmail.com>
2026-04-21 01:45:04 +0530

    connected frontend to deployed backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -7,7 +7,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000"
+  baseURL:"https://brainstorm-backend-m8li.onrender.com"
 })
 
 export default api
--- frontend/src/components/IdeaList.js
+++ frontend/src/components/IdeaList.js
@@ -3,7 +3,7 @@ import api from "../api/api"
 import VoteButton from "./VoteButton"
 import { io } from "socket.io-client"
 
-const socket = io("http://localhost:5000")
+const socket = io("https://brainstorm-backend-m8li.onrender.com")
 
 function IdeaList({ sessionId, user }) {
 
