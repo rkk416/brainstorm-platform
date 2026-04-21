097725550f3f7ab87948cc542c4828368b91f6c4
rkk416 <ramkrishn416@gmail.com>
2026-04-22 00:41:13 +0530

    Add userId parameter to createSession function

--- backend/src/models/sessionModel.js
+++ backend/src/models/sessionModel.js
@@ -1,22 +1,20 @@
 const db = require("../config/db")
 
-exports.createSession = async (title, description) => {
+exports.createSession = async (title, description, userId) => {
 
-const result = await db.query(
-"INSERT INTO sessions(title,description) VALUES($1,$2) RETURNING *",
-[title,description]
-)
+  const result = await db.query(
+    "INSERT INTO sessions(title,description,created_by) VALUES($1,$2,$3) RETURNING *",
+    [title, description, userId]
+  )
 
-return result.rows[0]
-
+  return result.rows[0]
 }
 
 exports.getSessions = async () => {
 
-const result = await db.query(
-"SELECT * FROM sessions ORDER BY id DESC"
-)
+  const result = await db.query(
+    "SELECT * FROM sessions ORDER BY id DESC"
+  )
 
-return result.rows
-
+  return result.rows
 }
