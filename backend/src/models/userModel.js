fffc6202e9a8658edb3b5e1c09dc62efa874c48a
rkk416 <ramkrishn416@gmail.com>
2026-04-22 00:32:46 +0530

    Refactor database connection in userModel.js

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,8 +1,8 @@
-const { pool } = require("../config/db")
+const db = require("../config/db")
 
 exports.createUser = async (name,email)=>{
 
-const result = await pool.query(
+const result = await db.query(
 "INSERT INTO users(name,email) VALUES($1,$2) RETURNING *",
 [name,email]
 )
